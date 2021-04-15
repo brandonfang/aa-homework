43ac221c67c7a568ccec24d22576b70e82e10ff7
brandonfang <bdmfang@gmail.com>
2021-04-15 07:42:21 -0700

    Update LRU cache

--- w5d1/lru_cache.rb
+++ w5d1/lru_cache.rb
@@ -1,41 +1,47 @@
 class LRUCache
-  def initialize
-    @cache = []
+  def initialize(size)
+    @store = []
+    @size = size
   end
 
   def count
-    @cache.length
+    @store.length
   end
 
   def add(ele)
-    @cache.delete(ele) if @cache.include?(ele)
-    @cache.push(ele)
+    @store.delete(ele) if @store.include?(ele)
+    if count >= @size
+      until count < @size
+        @store.shift 
+      end
+    end
+    @store.push(ele)
   end
 
   def show
-    @cache.first
+    p @store
   end
 
   private
-  # helper methods go here!
+  
 end
 
-# johnny_cache = LRUCache.new(4)
+johnny_cache = LRUCache.new(4)
 
-# johnny_cache.add("I walk the line")
-# johnny_cache.add(5)
+johnny_cache.add("I walk the line")
+johnny_cache.add(5)
 
-# johnny_cache.count # => returns 2
+p johnny_cache.count # => returns 2
 
-# johnny_cache.add([1,2,3])
-# johnny_cache.add(5)
-# johnny_cache.add(-5)
-# johnny_cache.add({a: 1, b: 2, c: 3})
-# johnny_cache.add([1,2,3,4])
-# johnny_cache.add("I walk the line")
-# johnny_cache.add(:ring_of_fire)
-# johnny_cache.add("I walk the line")
-# johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.add([1,2,3])
+johnny_cache.add(5)
+johnny_cache.add(-5)
+johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.add([1,2,3,4])
+johnny_cache.add("I walk the line")
+johnny_cache.add(:ring_of_fire)
+johnny_cache.add("I walk the line")
+johnny_cache.add({a: 1, b: 2, c: 3})
 
 
-# johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
+johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
