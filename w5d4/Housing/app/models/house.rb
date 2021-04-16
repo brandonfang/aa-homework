b6cf9894b518ba5fa67c6b3cbc4411a30bab7632
brandonfang <bdmfang@gmail.com>
2021-04-16 09:49:30 -0700

    Add w5d4

---
+++ w5d4/Housing/app/models/house.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id      :bigint           not null, primary key
+#  address :string           not null
+#
+class House < ApplicationRecord
+
+end
---
+++ w5d4/Housing/app/models/person.rb
@@ -0,0 +1,11 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id       :bigint           not null, primary key
+#  name     :string           not null
+#  house_id :integer          not null
+#
+class Person < ApplicationRecord
+
+end
