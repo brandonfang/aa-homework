76c6aeb2663857279e7ff736a641381b29e01dce
brandonfang <bdmfang@gmail.com>
2021-05-13 08:32:12 -0700

    Add w9d4

---
+++ w9d4/your-first-ajax-request/js/index.js
@@ -0,0 +1,21 @@
+console.log("Hello from the JavaScript console!");
+
+// Your AJAX request here
+
+$.ajax({
+  type: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  success: (data) => {
+    console.log(data);
+    console.log(
+      `In ${data.name}, the temperature is ${data.main.temp}`
+    );
+  },
+  error: () => {
+    console.log('There was an error')
+  }
+});
+
+// Add another console log here, outside your AJAX request
+
+console.log('Outside of our AJAX request');
